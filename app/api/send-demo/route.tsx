8014e4b5a579451c727b57cdf3091d8ac04e7c3b
Mohaned Elahwal <moha.decodo@gmail.com>
2026-05-08 10:42:25 -0400

    Update: send-demo API route

--- app/api/send-demo/route.tsx
+++ app/api/send-demo/route.tsx
@@ -7,7 +7,7 @@ import { DemoRequestEmail } from '@/components/emails/DemoRequestEmail'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SITE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
